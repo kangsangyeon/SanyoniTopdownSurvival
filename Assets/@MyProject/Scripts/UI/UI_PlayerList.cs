b7ea4909aa54525d337b01f8ab3e89a3df93ef04
DESKTOP-KSANGYE\KOCCA21 <ksyline1997@gmail.com>
2023-07-07 10:49:19 +0900

    fix: 서버 PC에서 PlayerList 에서 플레이어가 사라질 때 RemovePlayerUI 함수를 두 번 중복 호출해 문제가 발생하는 것을 없앱니다.

--- Assets/@MyProject/Scripts/UI/UI_PlayerList.cs
+++ Assets/@MyProject/Scripts/UI/UI_PlayerList.cs
@@ -1,9 +1,7 @@
 using System.Collections.Generic;
-using FishNet;
 using MyProject;
 using MyProject.Event;
 using UnityEngine;
-using UnityEngine.Events;
 using UnityEngine.UIElements;
 
 public class UI_PlayerList : MonoBehaviour
@@ -17,11 +15,6 @@ public class UI_PlayerList : MonoBehaviour
     private System.Action<PlayerAddedEventParam> m_OnPlayerAdded_OnClient;
     private System.Action<PlayerRemovedEventParam> m_OnPlayerRemoved_OnClient;
     private System.Action<PlayerKillEventParam> m_OnPlayerKill_OnClient;
-
-    private System.Action<Player> m_OnPlayerAdded_OnServer;
-    private System.Action<Player> m_OnPlayerRemoved_OnServer;
-    private System.Action<Player, Player> m_OnPlayerKill_OnServer;
-
     private System.Action m_OnPlayerRankRefreshed;
 
     public void Initialize()
@@ -29,55 +22,27 @@ public class UI_PlayerList : MonoBehaviour
         m_Parent = m_Document.rootVisualElement.Q("player-list");
 
         m_OnPlayerAdded_OnClient = _param => TryInitializePlayerUI(_param.player.connectionId);
+        m_GameScene.onPlayerAdded_OnClient += m_OnPlayerAdded_OnClient;
+        
         m_OnPlayerRemoved_OnClient = _param => RemovePlayerUI(_param.player.connectionId);
+        m_GameScene.onPlayerRemoved_OnClient += m_OnPlayerRemoved_OnClient;
+        
         m_OnPlayerKill_OnClient = param => RefreshPlayerUI(param.killer.connectionId);
-
+        m_GameScene.onPlayerKill_OnClient += m_OnPlayerKill_OnClient;
+        
         m_OnPlayerRankRefreshed = () => RefreshPlayerRankUI();
         m_GameScene.onPlayerRankRefreshed += m_OnPlayerRankRefreshed;
-
-
-        if (InstanceFinder.IsServer)
-        {
-            m_OnPlayerAdded_OnServer = p =>
-                m_OnPlayerAdded_OnClient(new PlayerAddedEventParam() { player = new PlayerInfo(p) });
-            m_OnPlayerRemoved_OnServer = p =>
-                m_OnPlayerRemoved_OnClient(new PlayerRemovedEventParam() { player = new PlayerInfo(p) });
-            m_OnPlayerKill_OnServer = (killer, target) =>
-                m_OnPlayerKill_OnClient(new PlayerKillEventParam() { killer = new PlayerInfo(killer), target = new PlayerInfo(target) });
-
-            m_GameScene.onPlayerAdded_OnServer += m_OnPlayerAdded_OnServer;
-            m_GameScene.onPlayerRemoved_OnServer += m_OnPlayerRemoved_OnServer;
-            m_GameScene.onPlayerKill_OnServer += m_OnPlayerKill_OnServer;
-        }
-        else
-        {
-            m_GameScene.onPlayerAdded_OnClient += m_OnPlayerAdded_OnClient;
-            m_GameScene.onPlayerRemoved_OnClient += m_OnPlayerRemoved_OnClient;
-            m_GameScene.onPlayerKill_OnClient += m_OnPlayerKill_OnClient;
-        }
     }
 
     public void Uninitialize()
     {
-        if (InstanceFinder.IsServer)
-        {
-            m_GameScene.onPlayerAdded_OnServer -= m_OnPlayerAdded_OnServer;
-            m_GameScene.onPlayerRemoved_OnServer -= m_OnPlayerRemoved_OnServer;
-            m_GameScene.onPlayerKill_OnServer -= m_OnPlayerKill_OnServer;
-
-            m_OnPlayerAdded_OnServer = null;
-            m_OnPlayerRemoved_OnServer = null;
-            m_OnPlayerKill_OnServer = null;
-        }
-        else
-        {
-            m_GameScene.onPlayerAdded_OnClient -= m_OnPlayerAdded_OnClient;
-            m_GameScene.onPlayerRemoved_OnClient -= m_OnPlayerRemoved_OnClient;
-            m_GameScene.onPlayerKill_OnClient -= m_OnPlayerKill_OnClient;
-        }
-
+        m_GameScene.onPlayerAdded_OnClient -= m_OnPlayerAdded_OnClient;
         m_OnPlayerAdded_OnClient = null;
+        
+        m_GameScene.onPlayerRemoved_OnClient -= m_OnPlayerRemoved_OnClient;
         m_OnPlayerRemoved_OnClient = null;
+        
+        m_GameScene.onPlayerKill_OnClient -= m_OnPlayerKill_OnClient;
         m_OnPlayerKill_OnClient = null;
 
         m_GameScene.onPlayerRankRefreshed -= m_OnPlayerRankRefreshed;
