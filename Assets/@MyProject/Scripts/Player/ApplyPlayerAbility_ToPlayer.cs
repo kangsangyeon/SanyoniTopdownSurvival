3a08a76badcb6d3ebf5eedc21dfc9b99731332d8
sanyoni <ksyline1997@gmail.com>
2023-07-28 05:25:30 +0900

    몇몇 능력치의 최저,최대 Clamp가 잘못 적용되어있던 문제를 수정합니다.

--- Assets/@MyProject/Scripts/Player/ApplyPlayerAbility_ToPlayer.cs
+++ Assets/@MyProject/Scripts/Player/ApplyPlayerAbility_ToPlayer.cs
@@ -11,28 +11,28 @@ namespace MyProject
             m_Player.health.maxHealth =
                 Mathf.Clamp(m_Player.abilityProperty.maxHealth
                             + m_Player.abilityProperty.maxHealthAddition,
-                    m_Player.abilityProperty.maximumMaxHealth,
-                    m_Player.abilityProperty.minimumMaxHealth);
+                    m_Player.abilityProperty.minimumMaxHealth,
+                    m_Player.abilityProperty.maximumMaxHealth);
 
             m_Player.movement.moveSpeed =
                 Mathf.Clamp(m_Player.abilityProperty.moveSpeed
                             + m_Player.abilityProperty.moveSpeedAddition,
-                    m_Player.abilityProperty.maximumMoveSpeed,
-                    m_Player.abilityProperty.minimumMoveSpeed);
+                    m_Player.abilityProperty.minimumMoveSpeed,
+                    m_Player.abilityProperty.maximumMoveSpeed);
 
             m_Player.onAbilityPropertyRefreshed_OnClient += () =>
             {
                 m_Player.health.maxHealth =
                     Mathf.Clamp(m_Player.abilityProperty.maxHealth
                                 + m_Player.abilityProperty.maxHealthAddition,
-                        m_Player.abilityProperty.maximumMaxHealth,
-                        m_Player.abilityProperty.minimumMaxHealth);
+                        m_Player.abilityProperty.minimumMaxHealth,
+                        m_Player.abilityProperty.maximumMaxHealth);
 
                 m_Player.movement.moveSpeed =
                     Mathf.Clamp(m_Player.abilityProperty.moveSpeed
                                 + m_Player.abilityProperty.moveSpeedAddition,
-                        m_Player.abilityProperty.maximumMoveSpeed,
-                        m_Player.abilityProperty.minimumMoveSpeed);
+                        m_Player.abilityProperty.minimumMoveSpeed,
+                        m_Player.abilityProperty.maximumMoveSpeed);
             };
         }
     }
